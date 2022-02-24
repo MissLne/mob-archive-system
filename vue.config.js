c43df9ba8fcef0664fe4ff61ce4c537ed2985a31
zhutuzhao <810870471@qq.com>
2022-02-25 01:02:04 +0800

    test: 测试github

--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,7 @@ module.exports = {
       // 配置跨域配置跨域http://192.168.3.9:8070  
       '/api': {
         // target: 'http://192.168.3.9:8070',
-        // target: 'https://test-archive.server.topviewclub.cn',
+        // target: 'https://test-archive.server.topviewclub.cn', 
         target: 'https://test-archive.aliyun.topviewclub.cn',
         // target: 'https://archive-system.aliyun.topviewclub.cn/',
         ws: true,
