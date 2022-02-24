bae8858529504956090f510f6f42fe2faef9a3cc
zhutuzhao <810870471@qq.com>
2022-02-25 00:48:16 +0800

    text: 测试github上传问题

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
     https: false,
     hotOnly: false,
     proxy: {
-      // 配置跨域配置跨域http://192.168.3.9:8070
+      // 配置跨域配置跨域http://192.168.3.9:8070 
       '/api': {
         // target: 'http://192.168.3.9:8070',
         // target: 'https://test-archive.server.topviewclub.cn',
